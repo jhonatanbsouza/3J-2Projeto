14e5350775bc9cdb505588428508b1ab330286ce
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-12-06 09:48:12 -0300

    Update quantidadeDeUsuarios.js

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont } from "./comum.js";
 
 async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -19,7 +19,34 @@ async function quantidadeDeUsuarios(){
     ]
     const layout = {
         plot_bgcolor: getCSS('--cor-de-fundo'),
-        paper_bgcolor: getCSS('--cor-de-fundo')
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title:{
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font:{
+                color: getCSS('--cor-primaria'),
+                family: getCSS('--fonte'),
+                size: 30
+            }
+        },
+        xaxis:{
+            tickfont: tickfont,
+            title:{
+                text: 'Nome das redes sociais',
+                font:{
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        },
+        yaxis:{
+            tickfont: tickfont,
+            title:{
+                text: 'Milhões de usuários ativos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        }
     }
     const grafico = document.createElement('div');
     grafico.className = 'grafico';
